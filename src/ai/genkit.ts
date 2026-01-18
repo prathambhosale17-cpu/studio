67b7b00088c5565f9cde7ef0a461939e1102e19a
Pratham Bhosale <prathambhosale17@gmail.com>
2026-01-18 16:26:05 +0000

    code is not connect to api servic

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,8 @@
+'use server';
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiVersion: 'v1beta'})],
-  model: 'gemini-2.5-flash',
+  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
+  model: 'gemini-1.5-flash-latest',
 });
