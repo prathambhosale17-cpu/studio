968acbee0bb4813c78b6ce20ac726bd714b001c3
Pratham Bhosale <prathambhosale17@gmail.com>
2026-01-19 14:04:29 +0000

    ## Error Type Console Error
    ## Error Message
    Failed to fetch from https

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -11,5 +11,5 @@ if (!process.env.GEMINI_API_KEY || process.env.GEMINI_API_KEY === 'AIzaSyDQcmGoH
 
 export const ai = genkit({
   plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
-  model: 'googleai/gemini-pro-vision',
+  model: 'googleai/gemini-1.5-flash-latest',
 });
